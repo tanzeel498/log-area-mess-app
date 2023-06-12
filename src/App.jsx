10f09d19f45d11c54d95ebc941f0375ca6e6887f
Tanzeel <tanzeel498@gmail.com>
2023-06-13 04:48:43 +0500

    add routes, setup react query along with devtools

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,60 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Toaster } from "react-hot-toast";
+
+import GloabalStyles from "./styles/GlobalStyles";
+import Login from "./pages/Login";
+import { DarkModeProvider } from "./context/DarkModeContext";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 100,
+    },
+  },
+});
+
 function App() {
-  return;
+  return (
+    <DarkModeProvider>
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <GloabalStyles />
+        <BrowserRouter>
+          <Routes>
+            <Route path="login" element={<Login />} />
+          </Routes>
+        </BrowserRouter>
+        <Toaster
+          position="top-center"
+          gutter={12}
+          containerStyle={{ margin: "8px" }}
+          toastOptions={{
+            success: {
+              duration: 3000,
+              style: {
+                backgroundColor: "var(--color-green-100)",
+                color: "var(--color-grey-700)",
+              },
+            },
+            error: {
+              duration: 5000,
+              style: {
+                backgroundColor: "var(--color-red-200)",
+                color: "#374151",
+              },
+            },
+            style: {
+              fontSize: "16px",
+              maxWidth: "500px",
+              padding: "16px 24px",
+            },
+          }}
+        />
+      </QueryClientProvider>
+    </DarkModeProvider>
+  );
 }
 
 export default App;
