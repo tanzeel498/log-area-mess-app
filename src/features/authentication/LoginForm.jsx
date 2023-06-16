19031f023c5b306ea82fdf414a4b3cbee117ae6a
Tanzeel <tanzeel498@gmail.com>
2023-06-16 20:36:10 +0500

    add login functionaity

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -3,16 +3,26 @@ import Button from "../../ui/Button";
 import Form from "../../ui/Form";
 import Input from "../../ui/Input";
 import FormRowVertical from "../../ui/FormRowVertical";
+import { useLogin } from "./useLogin";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
-  const isLoading = true;
+  const { login, isLoading } = useLogin();
 
   function handleSubmit(e) {
     e.preventDefault();
+    if (!email || !password) return;
+    login(
+      { email, password },
+      {
+        onSettled: () => {
+          setEmail("");
+          setPassword("");
+        },
+      }
+    );
   }
 
   return (
--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -1,3 +1,24 @@
-function useLogin() {}
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { login as loginApi } from "../../services/apiAuth";
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
 
-export default useLogin;
+export function useLogin() {
+  const navigate = useNavigate();
+  const queryClient = useQueryClient();
+
+  const {
+    mutate: login,
+    isLoading,
+    error,
+  } = useMutation({
+    mutationFn: loginApi,
+    onSuccess: (data) => {
+      queryClient.setQueryData(["user"], data.user);
+      navigate("/dashboard");
+    },
+    onError: (err) => toast.error(err.message),
+  });
+
+  return { login, isLoading, error };
+}
