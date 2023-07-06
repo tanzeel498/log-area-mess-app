46669f25ebedb832c9d5a294654e6e3644fb453c
Tanzeel <tanzeel498@gmail.com>
2023-07-06 23:03:14 +0500

    add booking detail component

---
+++ src/features/bookings/BookingDetail.jsx
@@ -0,0 +1,78 @@
+import styled from "styled-components";
+
+import BookingDataBox from "./BookingDataBox";
+import Row from "../../ui/Row";
+import Heading from "../../ui/Heading";
+import Tag from "../../ui/Tag";
+import ButtonGroup from "../../ui/ButtonGroup";
+import Button from "../../ui/Button";
+import ButtonText from "../../ui/ButtonText";
+import Spinner from "../../ui/Spinner";
+
+import { useMoveBack } from "../../hooks/useMoveBack";
+import { useBooking } from "./useBooking";
+import { useNavigate, useParams } from "react-router-dom";
+import { useCheckout } from "../check-in-out/useCheckout";
+import Empty from "../../ui/Empty";
+
+const HeadingGroup = styled.div`
+  display: flex;
+  gap: 2.4rem;
+  align-items: center;
+`;
+
+function BookingDetail() {
+  const { bookingId: bookingIdParam } = useParams();
+  const navigate = useNavigate();
+  const { booking, isLoading } = useBooking();
+  const moveBack = useMoveBack();
+  const { checkout, isCheckingOut } = useCheckout();
+
+  const statusToTagName = {
+    unconfirmed: "silver",
+    "checked-in": "green",
+    "checked-out": "red",
+  };
+
+  if (isLoading) return <Spinner />;
+  if (!booking)
+    return <Empty resourceName={`Booking with id: ${bookingIdParam}`} />;
+
+  const { id: bookingId, status } = booking;
+
+  return (
+    <>
+      <Row type="horizontal">
+        <HeadingGroup>
+          <Heading as="h1">Booking #{bookingId}</Heading>
+          <Tag type={statusToTagName[status]}>
+            {booking.status.replace("-", " ")}
+          </Tag>
+        </HeadingGroup>
+        <ButtonText onClick={moveBack}>&larr; Back</ButtonText>
+      </Row>
+
+      <BookingDataBox booking={booking} />
+
+      <ButtonGroup>
+        {status === "unconfirmed" && (
+          <Button onClick={() => navigate(`/checkin/${bookingId}`)}>
+            Check in
+          </Button>
+        )}
+
+        {status === "checked-in" && (
+          <Button onClick={() => checkout(bookingId)} disabled={isCheckingOut}>
+            Check out
+          </Button>
+        )}
+
+        <Button $variation="secondary" onClick={moveBack}>
+          Back
+        </Button>
+      </ButtonGroup>
+    </>
+  );
+}
+
+export default BookingDetail;
