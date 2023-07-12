2d01f4cf175942dcd21a16b04e9fa7da772a3b07
Tanzeel <tanzeel498@gmail.com>
2023-07-13 01:14:30 +0500

    add getCurrentUser Api

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -10,6 +10,20 @@ export async function login({ email, password }) {
   return data;
 }
 
+export async function getCurrentUser() {
+  const { data } = await supabase.auth.getSession();
+
+  if (!data.session) return null;
+
+  const {
+    data: { user },
+    error,
+  } = await supabase.auth.getUser();
+
+  if (error) throw new Error(error.message);
+  return user;
+}
+
 export async function logout() {
   const { error } = await supabase.auth.signOut();
   if (error) throw new Error(error.message);
