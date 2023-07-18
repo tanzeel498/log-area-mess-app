afa25cb40b68e14379ebf6f50b03255f2b6b4c50
Tanzeel <tanzeel498@gmail.com>
2023-07-19 01:24:17 +0500

    add useUpdateSettings hook

---
+++ src/features/settings/useUpdateSetting.js
@@ -0,0 +1,18 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { updateSetting as updateSettingApi } from "../../services/apiSettings";
+import toast from "react-hot-toast";
+
+export function useUpdateSetting() {
+  const queryClient = useQueryClient();
+
+  const { mutate: updateSetting, isLoading: isUpdating } = useMutation({
+    mutationFn: updateSettingApi,
+    onSuccess: () => {
+      queryClient.invalidateQueries(["settings"]);
+      toast.success("Setting updated successfully!");
+    },
+    onError: (err) => toast.error(err.message),
+  });
+
+  return { updateSetting, isUpdating };
+}
