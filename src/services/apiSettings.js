4dc6ed54ac7ab6cfcd84f8de814118ab9b3f5a5e
Tanzeel <tanzeel498@gmail.com>
2023-07-20 01:25:10 +0500

    add getSettings Api

---
+++ src/services/apiSettings.js
@@ -0,0 +1,11 @@
+import supabase from "./supabase";
+
+export async function getSettings() {
+  const { data, error } = await supabase.from("settings").select("*").single();
+
+  if (error) {
+    console.error(error);
+    throw new Error("Settings could not be loaded");
+  }
+  return data;
+}
