5bcdaeba9e738bdb578c63e8a8d8b25679ab29ae
Tanzeel <tanzeel498@gmail.com>
2023-08-10 01:45:10 +0500

    add updateUser Api

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -40,6 +40,39 @@ export async function getCurrentUser() {
   return user;
 }
 
+export async function updateCurrentUser({ fullName, avatar, password }) {
+  let dataToUpdate; // we have made two seperate forms for Name and password so we can receive only 1 at a time
+  if (password) dataToUpdate = { password };
+  else if (fullName) dataToUpdate = { data: { fullName } };
+
+  const { data, error } = await supabase.auth.updateUser(dataToUpdate);
+
+  if (error) throw new Error(error.message);
+  if (!avatar) return data;
+
+  // Addressing the avatar if uploaded
+  // 1. uploading the image to storage --> avatars bucket
+  const fileName = `avatar-${data.user.id}-${avatar.name}`;
+  const { error: imageUploadError } = await supabase.storage
+    .from("avatars")
+    .upload(fileName, avatar);
+
+  if (imageUploadError)
+    if (imageUploadError.message !== "The resource already exists")
+      throw new Error(imageUploadError.message);
+
+  // 2. Updating this new image src into the user data
+  const { data: userUpdateData, error: userUpdateError } =
+    await supabase.auth.updateUser({
+      data: {
+        avatar: `${supabaseUrl}/storage/v1/object/public/avatars/${fileName}`,
+      },
+    });
+  if (userUpdateError) throw new Error(imageUploadError.message);
+
+  return userUpdateData;
+}
+
 export async function logout() {
   const { error } = await supabase.auth.signOut();
   if (error) throw new Error(error.message);
