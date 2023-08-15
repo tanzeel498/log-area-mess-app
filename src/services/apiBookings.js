1237e01a9aaab6e8bc09208cba4b2d06c63e23cb
Tanzeel <tanzeel498@gmail.com>
2023-08-16 02:00:59 +0500

    add staysAfterDate Api

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -174,3 +174,19 @@ export async function getBookingsAfterDate(date) {
 
   return data;
 }
+
+// Returns all STAYS that were created after the given date
+export async function getStaysAfterDate(date) {
+  const { data, error } = await supabase
+    .from("bookings")
+    .select("*, guests(fullName)")
+    .gte("startDate", date)
+    .lte("startDate", getToday());
+
+  if (error) {
+    console.error(error);
+    throw new Error("Bookings could not get loaded");
+  }
+
+  return data;
+}
