8bc211dac1ea7346e9d0bac44ada71f2fec80376
Tanzeel <tanzeel498@gmail.com>
2023-08-16 02:01:39 +0500

    add getStays for today activity

--- src/services/apiBookings.js
+++ src/services/apiBookings.js
@@ -190,3 +190,24 @@ export async function getStaysAfterDate(date) {
 
   return data;
 }
+
+// Activity means that there is a check in or a check out today
+export async function getStaysTodayActivity() {
+  const { data, error } = await supabase
+    .from("bookings")
+    .select("*, guests(fullName, formation, serviceNumber)")
+    .or(
+      `and(status.eq.unconfirmed,startDate.eq.${getToday()}),and(status.eq.checked-in,endDate.eq.${getToday()})`
+    )
+    .order("created_at");
+
+  // Equivalent to this. But by querying this, we only download the data we actually need, otherwise we would need ALL bookings ever created
+  // (stay.status === 'unconfirmed' && isToday(new Date(stay.startDate))) ||
+  // (stay.status === 'checked-in' && isToday(new Date(stay.endDate)))
+
+  if (error) {
+    console.error(error);
+    throw new Error("Bookings could not get loaded");
+  }
+  return data;
+}
