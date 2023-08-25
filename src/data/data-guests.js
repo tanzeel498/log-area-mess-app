684588ac37e7b8ec2979c447cf3e18d16016fa6a
Tanzeel <tanzeel498@gmail.com>
2023-08-26 02:36:14 +0500

    add guests data

---
+++ src/data/data-guests.js
@@ -0,0 +1,187 @@
+// Search for: 'jo', 'fa', 'mar', 'emm', 'ah'
+
+export const guests = [
+  {
+    // id: 1000,
+    fullName: "Jonas Schmedtmann",
+    email: "hello@jonas.io",
+    formation: "Log Area Rwp",
+    serviceNumber: "3525436345",
+  },
+  {
+    fullName: "Jonathan Smith",
+    email: "johnsmith@test.eu",
+    formation: "10 div",
+    serviceNumber: "4534593454",
+  },
+  {
+    fullName: "Jonatan Johansson",
+    email: "jonatan@example.com",
+    formation: "2 div",
+    serviceNumber: "9374074454",
+  },
+  {
+    fullName: "Jonas Mueller",
+    email: "jonas@example.eu",
+    formation: "10 corps HQ",
+    serviceNumber: "1233212288",
+  },
+  {
+    fullName: "Jonas Anderson",
+    email: "anderson@example.com",
+    formation: "1 corps HQ",
+    serviceNumber: "0988520146",
+  },
+  {
+    fullName: "Jonathan Williams",
+    email: "jowi@gmail.com",
+    formation: "30 corps HQ",
+    serviceNumber: "633678543",
+  },
+
+  // GPT
+  {
+    fullName: "Emma Watson",
+    email: "emma@gmail.com",
+    formation: "6 div",
+    serviceNumber: "1234578901",
+  },
+  {
+    fullName: "Mohammed Ali",
+    email: "mohammedali@yahoo.com",
+    formation: "17 div",
+    serviceNumber: "987543210",
+  },
+  {
+    fullName: "Maria Rodriguez",
+    email: "maria@gmail.com",
+    formation: "37 div",
+    serviceNumber: "1098765321",
+  },
+  {
+    fullName: "Li Mei",
+    email: "li.mei@hotmail.com",
+    formation: "28 div",
+    serviceNumber: "102934756",
+  },
+  {
+    fullName: "Khadija Ahmed",
+    email: "khadija@gmail.com",
+    formation: "10 Armd Bgd",
+    serviceNumber: "1023457890",
+  },
+  {
+    fullName: "Gabriel Silva",
+    email: "gabriel@gmail.com",
+    formation: "23 Indp Inf Bgd",
+    serviceNumber: "109283465",
+  },
+  {
+    fullName: "Maria Gomez",
+    email: "maria@example.com",
+    formation: "87 div",
+    serviceNumber: "108765421",
+  },
+  {
+    fullName: "Ahmed Hassan",
+    email: "ahmed@gmail.com",
+    formation: "12 corps HQ",
+    serviceNumber: "1077777777",
+  },
+  {
+    fullName: "John Doe",
+    email: "johndoe@gmail.com",
+    formation: "40 corps",
+    serviceNumber: "3245908744",
+  },
+  {
+    fullName: "Fatima Ahmed",
+    email: "fatima@example.com",
+    formation: "34 div",
+    serviceNumber: "1089999363",
+  },
+  {
+    fullName: "David Smith",
+    email: "david@gmail.com",
+    formation: "29 div",
+    serviceNumber: "44450960283",
+  },
+  {
+    fullName: "Marie Dupont",
+    email: "marie@gmail.com",
+    formation: "Log Area Gwa",
+    serviceNumber: "06934233728",
+  },
+  {
+    fullName: "Ramesh Patel",
+    email: "ramesh@gmail.com",
+    formation: "Log Area Kci",
+    serviceNumber: "9875412303",
+  },
+  {
+    fullName: "Fatimah Al-Sayed",
+    email: "fatimah@gmail.com",
+    formation: "50 div",
+    serviceNumber: "0123456789",
+  },
+  {
+    fullName: "Nina Williams",
+    email: "nina@hotmail.com",
+    formation: "Log Area Mlt",
+    serviceNumber: "2345678901",
+  },
+  {
+    fullName: "Taro Tanaka",
+    email: "taro@gmail.com",
+    formation: "Log Area psw",
+    serviceNumber: "3456789012",
+  },
+  {
+    fullName: "Abdul Rahman",
+    email: "abdul@gmail.com",
+    formation: "194 div",
+    serviceNumber: "4567890123",
+  },
+  {
+    fullName: "Julie Nguyen",
+    email: "julie@gmail.com",
+    formation: "5 div HQ",
+    serviceNumber: "5678901234",
+  },
+  {
+    fullName: "Sara Lee",
+    email: "sara@gmail.com",
+    formation: "29 div",
+    serviceNumber: "6789012345",
+  },
+  {
+    fullName: "Carlos Gomez",
+    email: "carlos@yahoo.com",
+    formation: "60 div",
+    serviceNumber: "7890123456",
+  },
+  {
+    fullName: "Emma Brown",
+    email: "emma@gmail.com",
+    formation: "39 div",
+    serviceNumber: "8901234567",
+  },
+  {
+    fullName: "Juan Hernandez",
+    email: "juan@yahoo.com",
+    formation: "34 Armd Bgd",
+    serviceNumber: "4343433333",
+  },
+  {
+    fullName: "Ibrahim Ahmed",
+    email: "ibrahim@yahoo.com",
+    formation: "80 div",
+    serviceNumber: "2345678009",
+  },
+  {
+    fullName: "Mei Chen",
+    email: "mei@gmail.com",
+    formation: "Log Area Gwa",
+    serviceNumber: "3456117890",
+  },
+];
