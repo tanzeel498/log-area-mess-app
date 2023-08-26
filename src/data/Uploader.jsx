305f1bd279b49d5cc92c93da12c76197c84d857c
Tanzeel <tanzeel498@gmail.com>
2023-08-27 02:37:09 +0500

    add data uploader component

---
+++ src/data/Uploader.jsx
@@ -0,0 +1,148 @@
+import { useState } from "react";
+import { isFuture, isPast, isToday } from "date-fns";
+import supabase from "../services/supabase";
+import Button from "../ui/Button";
+import { subtractDates } from "../utils/helpers";
+
+import { bookings } from "./data-bookings";
+import { guestRooms } from "./data-guest-rooms";
+import { guests } from "./data-guests";
+
+async function deleteGuests() {
+  const { error } = await supabase.from("guests").delete().gt("id", 0);
+  if (error) console.log(error.message);
+}
+
+async function deleteGuestRooms() {
+  const { error } = await supabase.from("guest-rooms").delete().gt("id", 0);
+  if (error) console.log(error.message);
+}
+
+async function deleteBookings() {
+  const { error } = await supabase.from("bookings").delete().gt("id", 0);
+  if (error) console.log(error.message);
+}
+
+async function createGuests() {
+  const { error } = await supabase.from("guests").insert(guests);
+  if (error) console.log(error.message);
+}
+
+async function createGuestRooms() {
+  const { error } = await supabase.from("guest-rooms").insert(guestRooms);
+  if (error) console.log(error.message);
+}
+
+async function createBookings() {
+  // Bookings need a guestId and a guestRoomId. We can't tell Supabase IDs for each object, it will calculate them on its own. So it might be different for different people, especially after multiple uploads. Therefore, we need to first get all guestIds and guestRoomIds, and then replace the original IDs in the booking data with the actual ones from the DB
+  const { data: guestsIds } = await supabase
+    .from("guests")
+    .select("id")
+    .order("id");
+  const allGuestIds = guestsIds.map((guestRoom) => guestRoom.id);
+  const { data: guestRoomsIds } = await supabase
+    .from("guest-rooms")
+    .select("id")
+    .order("id");
+  const allGuestRoomIds = guestRoomsIds.map((guestRoom) => guestRoom.id);
+
+  const finalBookings = bookings.map((booking) => {
+    // Here relying on the order of guestRooms, as they don't have and ID yet
+    const guestRoom = guestRooms.at(booking.guestRoomId - 1);
+    const numNights = subtractDates(booking.endDate, booking.startDate);
+    const guestRoomPrice =
+      numNights * (guestRoom.regularPrice - guestRoom.discount);
+    const extrasPrice = booking.hasBreakfast
+      ? numNights * 15 * booking.numGuests
+      : 0; // hardcoded breakfast price
+    const totalPrice = guestRoomPrice + extrasPrice;
+
+    let status;
+    if (
+      isPast(new Date(booking.endDate)) &&
+      !isToday(new Date(booking.endDate))
+    )
+      status = "checked-out";
+    if (
+      isFuture(new Date(booking.startDate)) ||
+      isToday(new Date(booking.startDate))
+    )
+      status = "unconfirmed";
+    if (
+      (isFuture(new Date(booking.endDate)) ||
+        isToday(new Date(booking.endDate))) &&
+      isPast(new Date(booking.startDate)) &&
+      !isToday(new Date(booking.startDate))
+    )
+      status = "checked-in";
+
+    return {
+      ...booking,
+      numNights,
+      guestRoomPrice,
+      extrasPrice,
+      totalPrice,
+      guestId: allGuestIds.at(booking.guestId - 1),
+      guestRoomId: allGuestRoomIds.at(booking.guestRoomId - 1),
+      status,
+    };
+  });
+
+  console.log(finalBookings);
+
+  const { error } = await supabase.from("bookings").insert(finalBookings);
+  if (error) console.log(error.message);
+}
+
+function Uploader() {
+  const [isLoading, setIsLoading] = useState(false);
+
+  async function uploadAll() {
+    setIsLoading(true);
+    // Bookings need to be deleted FIRST
+    await deleteBookings();
+    await deleteGuests();
+    await deleteGuestRooms();
+
+    // Bookings need to be created LAST
+    await createGuests();
+    await createGuestRooms();
+    await createBookings();
+
+    setIsLoading(false);
+  }
+
+  async function uploadBookings() {
+    setIsLoading(true);
+    await deleteBookings();
+    await createBookings();
+    setIsLoading(false);
+  }
+
+  return (
+    <div
+      style={{
+        marginTop: "auto",
+        backgroundColor: "#e0e7ff",
+        padding: "8px",
+        borderRadius: "5px",
+        textAlign: "center",
+        display: "flex",
+        flexDirection: "column",
+        gap: "8px",
+      }}
+    >
+      <h3>SAMPLE DATA</h3>
+
+      <Button onClick={uploadAll} disabled={isLoading}>
+        Upload ALL
+      </Button>
+
+      <Button onClick={uploadBookings} disabled={isLoading}>
+        Upload bookings ONLY
+      </Button>
+    </div>
+  );
+}
+
+export default Uploader;
